106ab70d8cb0d2fdc6a5bbdc1f460b8809daf828
Gioakk <gioakbombaci@gmail.com>
2015-01-22 12:33:44 +0100

    aggiunta classe per interrogazione tabelle observations.

--- restServerHive.py
+++ restServerHive.py
@@ -12,8 +12,14 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
 urls = (
-    '/taxi/Taxi1', 'index'
+    '/taxi/Taxi1', 'get_taxi_obs',
+    '/observations', 'get_observation'	
 )
+
+class Row_obs:
+    def __init__(self, date, value):
+        self.date = date
+        self.value = value
 
 class Row:
     def __init__(self, date, temperature, pressure, pm10):
@@ -22,7 +28,7 @@ class Row:
 	self.pressure = pressure
 	self.pm10 = pm10	
 
-class index:
+class get_taxi_obs:
     def GET(self):
     	#return "Hello, world!"
 	try:
@@ -65,18 +71,81 @@ class index:
 			currentRow = Row(date, temperature, pressure, pm10)
 			provaJ = json.dumps(currentRow.__dict__)
 			print provaJ
-			#print provaJ.pressure
 			print currentRow.temperature		
 			table.append(provaJ)
 			cont = cont + 1
 			# whatever you want to do with this row, here
 			result = result + row + "\n"
+
+
 		#print client.fetchAll()
 		transport.close()
 		provaJtable = json.dumps(table)
 		return provaJtable
 	except Thrift.TException, tx:
 		return tx.message
+
+class get_observation:
+	def GET(self):
+		try:
+		        transport = TSocket.TSocket('130.206.80.46', 10000)
+               		transport = TTransport.TBufferedTransport(transport)
+                	protocol = TBinaryProtocol.TBinaryProtocol(transport)
+                	result = ""
+
+                	client = ThriftHive.Client(protocol)
+                	transport.open()
+
+			parameter = web.input()
+			ent_id = parameter.id
+			ent_type = parameter.type
+                	client.execute("select * from gioakbombaci_"+ent_id+"_"+ent_type)
+
+                	table = []
+               		date = ""
+                	value = ""
+			cont = 0
+ 	                while (1):
+             	           	row = client.fetchOne()
+
+                	        if (row == ""):
+       	                 		break
+	
+                        	r_o_w = row.split()
+				if(len(r_o_w)!=2):
+					continue
+                        	if(r_o_w[0]):
+                                	print r_o_w[0]
+                                	date = r_o_w[0]
+                        	if(r_o_w[1]):
+                                	print r_o_w[1]
+                                	value = r_o_w[1]
+				"""
+                        	if(r_o_w[2]):
+                                	print r_o_w[2]
+                                	pressure = r_o_w[2]
+                        	if(r_o_w[3]):
+                                	print r_o_w[3]
+                                	pm10 = r_o_w[3]
+				"""
+
+                        	currentRow = Row_obs(date, value)
+                        	provaJ = json.dumps(currentRow.__dict__)
+                        	print provaJ
+                        	print currentRow.value
+                        	table.append(provaJ)
+                        	cont = cont + 1
+                        	# whatever you want to do with this row, here
+                        	result = result + row + "\n"
+
+                	#print client.fetchAll()
+                	transport.close()
+                	provaJtable = json.dumps(table)
+                	return provaJtable
+        	except Thrift.TException, tx:
+                	return tx.message
+
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
